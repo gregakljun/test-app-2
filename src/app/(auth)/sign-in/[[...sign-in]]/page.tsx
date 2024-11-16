b2d40d8a2e69700132cb5b319c57b956859d5377
gregakljun <138165368+gregakljun@users.noreply.github.com>
2024-11-16 19:31:47 +0100

    Update page.tsx

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,3 +3,10 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return <SignIn />;
 }
+
+export function generateStaticParams() {
+  return [
+    // If you have possible values for your dynamic segment (e.g. `sign-in/something`), include them here.
+    { signIn: ['/sign-in'] },  // Example static params for the route
+  ];
+}
