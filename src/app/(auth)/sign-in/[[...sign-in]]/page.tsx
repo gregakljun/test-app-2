02fd66c12c0ff596a82ef408f8a33b456c119b73
gregakljun <138165368+gregakljun@users.noreply.github.com>
2024-11-16 19:19:34 +0100

    Update page.tsx

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -3,12 +3,3 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return <SignIn />;
 }
-
-async function generateStaticParams() {
-  // Define the static paths for this route
-  return [
-    { signIn: [""] },          // Matches `/` or `/sign-in`
-    { signIn: ['example'] }, // Matches `/sign-in/example`
-    // Add more paths as needed
-  ];
-}
