b9af4df4f83c2bd7074add35125a9dd8fac8cd10
gregakljun <138165368+gregakljun@users.noreply.github.com>
2024-11-16 17:55:33 +0100

    Update page.tsx

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -7,7 +7,7 @@ export default function SignUpPage() {
 // You need to provide generateStaticParams for dynamic routes
 export function generateStaticParams() {
   return [
-    // If you have possible values for your dynamic segment (e.g. `sign-in/something`), include them here.
+    // If you have possible values for your dynamic segment (e.g. `sign-iup/something`), include them here.
     { signUp: [] },  // Example static params for the route
   ];
 }
