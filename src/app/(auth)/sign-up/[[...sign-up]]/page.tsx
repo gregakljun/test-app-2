76b0f8e6107b81d5e39018aa259294423f65acd9
gregakljun <138165368+gregakljun@users.noreply.github.com>
2024-11-16 19:03:06 +0100

    Update page.tsx

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,13 +1,13 @@
-import { SignUp } from "@clerk/nextjs"
+import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
-  return <SignUp />
+  return <SignUp />;
 }
 
-// You need to provide generateStaticParams for dynamic routes
-export default function generateStaticParams() {
+// Named export for generateStaticParams
+export function generateStaticParams() {
   return [
-    // If you have possible values for your dynamic segment (e.g. `sign-iup/something`), include them here.
-    { signUp: [] },  // Example static params for the route
+    // If you have possible values for your dynamic segment (e.g. `sign-up/something`), include them here.
+    { signUp: [] }, // Example static params for the route
   ];
 }
